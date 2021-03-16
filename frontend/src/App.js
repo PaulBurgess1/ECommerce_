61816807cdc8cc4aba2a67ddd9db870661ec63b0
PaulBurgess <burg0920@mylaurier.ca>
2021-03-16 14:11:57 -0400

    products render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
 
 function App() {
 
@@ -34,51 +35,20 @@ function App() {
     <main className="main">
         <div className="content">
             <ul className="products">
-                <li>
+                {
+                data.products.map(product => 
+                    <li>
                     <div className="product">
-                        <img className="product-image" src="/images/p1.jpg" alt="product"></img>
-                        <div className="product-name"><a href="product.html">Shirt</a></div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$40.00</div>
-                        <div className="product-rating">4.5 Stars (3 Reviews)</div>
+                        <img className="product-image" src={product.image} alt="product"></img>
+                        <div className="product-name"><a href="product.html">{product.name}</a></div>
+                        <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">${product.price}</div>
+                        <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
                     </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="/images/p1.jpg" alt="product"></img>
-                        <div className="product-name"><a href="product.html">Shirt</a></div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$40.00</div>
-                        <div className="product-rating">4.5 Stars (3 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="/images/p1.jpg" alt="product"></img>
-                        <div className="product-name"><a href="product.html">Shirt</a></div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$40.00</div>
-                        <div className="product-rating">4.5 Stars (3 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="/images/p1.jpg" alt="product"></img>
-                        <div className="product-name"><a href="product.html">Shirt</a></div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$40.00</div>
-                        <div className="product-rating">4.5 Stars (3 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="/images/p1.jpg" alt="product"></img>
-                        <div className="product-name"><a href="product.html">Shirt</a></div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$40.00</div>
-                        <div className="product-rating">4.5 Stars (3 Reviews)</div>
-                    </div>
-                </li>
+                </li>)
+                }
+                
+                
             </ul>
         </div>
     </main>
---
+++ frontend/src/data.js
@@ -0,0 +1,41 @@
+export default {
+    products:[
+        {
+        name: "Shirt",
+        category: "Shirts",
+        image: "/images/p1.jpg",
+        price:40.00,
+        brand:"Nike",
+        rating: 3.5,
+        numReviews: 3
+    },
+    {
+        name: "Shirt 2",
+        category: "Shirts",
+        image: "/images/p1.jpg",
+        price:40.00,
+        brand:"Nike",
+        rating: 3.5,
+        numReviews: 3
+    },
+    {
+        name: "Shirt 3",
+        category: "Shirts",
+        image: "/images/p1.jpg",
+        price:40.00,
+        brand:"Nike",
+        rating: 3.5,
+        numReviews: 3
+    },
+    {
+        name: "Shirt 4",
+        category: "Shirts",
+        image: "/images/p1.jpg",
+        price:40.00,
+        brand:"Nike",
+        rating: 3.5,
+        numReviews: 3
+    },
+]
+
+}
